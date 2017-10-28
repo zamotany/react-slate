898af30e481b52fe985fddd06577fce8ede898b5
Paweł Trysła <pawel.trysla@gmail.com>
2017-10-28 17:38:19 +0200

    refactor Layout module and write tests for it

--- src/utils/Layout.js
+++ src/utils/Layout.js
@@ -2,22 +2,6 @@
 
 import type { Element } from '../types';
 
-function getLines(text) {
-  let lines = text.split('\n');
-
-  if (lines.length === 1) {
-    return lines;
-  }
-
-  const endsWithNewLine = text.endsWith('\n');
-  if (endsWithNewLine) {
-    lines = lines.slice(0, lines.length - 1);
-  }
-  return lines.map(
-    (line, i) => `${line}${i < lines.length - 1 || endsWithNewLine ? '\n' : ''}`
-  );
-}
-
 export default class Layout {
   elements: Element[] = [];
   lines: string[] = [];
@@ -30,38 +14,47 @@ export default class Layout {
   fillMissingLines(toLine: number) {
     const LINES_LENGTH = this.lines.length;
     for (let i = this.lastFilledLine; i < toLine; i++) {
-      this.lines[i] = i < LINES_LENGTH ? this.lines[i] : '\n';
+      this.lines[i] = i < LINES_LENGTH ? this.lines[i] : '';
     }
     this.lastFilledLine = toLine;
   }
 
   build() {
     // @TODO: support layers (z-index)
-
     // Assumption: both x and y are zero-based indexes
     this.elements.forEach(element => {
-      const contentLines = getLines(element.text);
-      const TOP_OFFSET = element.y + element.parentsOffsetY;
-      const LEFT_OFFSET = element.x + element.parentsOffsetX;
-      if (TOP_OFFSET + contentLines.length > this.lines.length) {
+      const textByLines = element.text.split('\n');
+      const POS_Y = getPositionY(element);
+      const POS_X = getPositionX(element);
+
+      if (POS_Y + textByLines.length > this.lines.length) {
         // TODO: get rid of it
-        this.fillMissingLines(TOP_OFFSET + contentLines.length);
+        this.fillMissingLines(POS_Y + textByLines.length);
       }
 
-      contentLines.forEach((content, i) => {
-        const line =
-          this.lines[TOP_OFFSET + i] === '\n' ? '' : this.lines[TOP_OFFSET + i];
-        const postContent = line.substr(LEFT_OFFSET + content.length);
+      textByLines.forEach((textLine, lineIndex) => {
+        const line = this.lines[POS_Y + lineIndex];
+        const postContent = line.substr(POS_X + textLine.length);
 
-        let preContent = line.substr(0, LEFT_OFFSET);
-        if (preContent.length < LEFT_OFFSET) {
-          preContent += ' '.repeat(LEFT_OFFSET - preContent.length);
+        let preContent = line.substr(0, POS_X);
+        if (preContent.length < POS_X) {
+          preContent += ' '.repeat(POS_X - preContent.length);
         }
 
-        this.lines[TOP_OFFSET + i] = `${preContent}${content}${postContent}`;
+        this.lines[
+          POS_Y + lineIndex
+        ] = `${preContent}${textLine}${postContent}`;
       });
     });
 
-    return this.lines.join('');
+    return this.lines.join('\n');
   }
+}
+
+function getPositionY(element: Element) {
+  return element.y + element.parentsOffsetY;
+}
+
+function getPositionX(element: Element) {
+  return element.x + element.parentsOffsetX;
 }
