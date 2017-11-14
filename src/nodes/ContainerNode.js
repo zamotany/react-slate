55c2a1f1a5d8a8a1c16409a2733777ae216a84bc
Paweł Trysła <pawel.trysla@gmail.com>
2017-11-14 21:33:26 +0100

    remove obsolete logic

--- src/nodes/ContainerNode.js
+++ src/nodes/ContainerNode.js
@@ -10,8 +10,6 @@ import {
 import ChunkNode from './ChunkNode';
 import { mergeCanvas, getCanvas } from '../utils/layout';
 
-import type { Element } from '../types';
-
 type Options = {
   // @TODO: add clearOnExit option
   debug: boolean,
@@ -24,7 +22,6 @@ type Options = {
 
 export default class ContainerNode {
   children: ChunkNode[] = [];
-  elements: Element[] = [];
   frontBuffer: string = '';
   backBuffer: string = '';
   stream: any = null;
@@ -88,62 +85,9 @@ export default class ContainerNode {
     this.children.splice(index, 1);
   }
 
-  appendElement(element: Element) {
-    this.elements.push(element);
-  }
-
-  diffBuffers() {
-    if (!this.options.renderOptimizations || !this.backBuffer.length) {
-      return 0;
-    }
-
-    const backBuffer = this.backBuffer.split('\n');
-    const frontBuffer = this.frontBuffer.split('\n');
-
-    let index = 0;
-    for (const frontLine of frontBuffer) {
-      if (backBuffer[index] !== frontLine) {
-        break;
-      }
-      index++;
-    }
-
-    return index;
-  }
-
-  // withDebugInfo(body: string, { splitPoint }: { splitPoint: number }) {
-  //   let debugInfo = `${'='.repeat(10)} DEBUG ${'='.repeat(10)}\n`;
-  //   debugInfo += `  frontBuffer.length: ${this.backBuffer.length}\n`;
-  //   debugInfo += `  backBuffer.length: ${this.frontBuffer.length}\n`;
-  //   debugInfo += `  renderOptimizations: ${this.options.renderOptimizations
-  //     ? 'enabled'
-  //     : 'disabled'}\n`;
-  //   debugInfo += `  frontBuffer splitPoint: ${splitPoint}\n`;
-  //   return `${body}\n\n${debugInfo}`;
-  // }
-
-  getOutput(splitPoint?: number) {
-    let body;
-    if (splitPoint) {
-      body = `${this.frontBuffer
-        .split('\n')
-        .slice(splitPoint)
-        .join('\n')}\n`;
-    } else {
-      body = `${this.frontBuffer}\n`;
-    }
-
-    // if (this.options.debug) {
-    //   return this.withDebugInfo(body, { splitPoint: splitPoint || 0 });
-    // }
-    return body;
-  }
-
   flush() {
     // @TODO: this buffer/optimization/slitting logic needs to be refactored
-    this.backBuffer = this.frontBuffer.split('\n').join('\n');
-    this.elements = [];
-
+    // @TODO: draw damage to screen only instead of everything
     const canvas = getCanvas(this.canvasSize);
     this.frontBuffer = mergeCanvas(
       canvas,
@@ -153,14 +97,8 @@ export default class ContainerNode {
       )
     ).join('\n');
 
-    if (this.backBuffer === this.frontBuffer) {
-      return;
-    }
-
-    const splitPoint = this.diffBuffers();
-
-    readline.cursorTo(this.stream, 0, splitPoint);
+    readline.cursorTo(this.stream, 0, 0);
     readline.clearScreenDown(this.stream);
-    this.stream.write(this.getOutput(splitPoint));
+    this.stream.write(this.frontBuffer);
   }
 }
