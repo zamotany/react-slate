32089a1d51285f9118c3e02696d96bc80b5405df
Paweł Trysła <pawel.trysla@gmail.com>
2017-12-12 15:45:03 +0100

    add ProgressBar component

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,9 +1,45 @@
 /* @flow */
 
 import React from 'react';
+import Text from './Text';
 
-export default class ProgressBar extends React.Component<void> {
-  render() {
-    return null;
-  }
+type CharProps = {
+  char?: string,
+  style?: any,
+};
+
+type Props = {
+  value: number,
+  barWidth: number,
+  chars?: {
+    open?: CharProps,
+    close?: CharProps,
+    bar?: CharProps,
+    fill?: CharProps,
+  },
+  style?: any,
+};
+
+export default function ProgressBar(props: Props) {
+  const { open = {}, close = {}, bar = {}, fill = {} } = props.chars || {};
+  const value = Math.min(props.value, 1);
+  const width = props.barWidth - 2;
+  const progress = Math.round(value * width);
+
+  return (
+    <Text style={{ display: 'inline', ...(props.style || {}) }}>
+      <Text style={{ display: 'inline', ...(open.style || {}) }}>
+        {open.char || '['}
+      </Text>
+      <Text style={{ display: 'inline', ...(bar.style || {}) }}>
+        {(bar.char || '=').repeat(progress)}
+      </Text>
+      <Text style={{ display: 'inline', ...(fill.style || {}) }}>
+        {(fill.char || ' ').repeat(width - progress)}
+      </Text>
+      <Text style={{ display: 'inline', ...(close.style || {}) }}>
+        {close.char || ']'}
+      </Text>
+    </Text>
+  );
 }
