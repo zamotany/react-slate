5b9100ff2dbbbb9b40460d7ea06adb029537f3e3
Paweł Trysła <pawel.trysla@gmail.com>
2017-12-12 15:56:05 +0100

    add ios/android compilation progress bars

--- example/src/haulCLI/components/Status.js
+++ example/src/haulCLI/components/Status.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // eslint-disable-next-line
-import { Text, Endl } from 'react-stream-renderer';
+import { Text, ProgressBar } from 'react-stream-renderer';
 
 function StatusItem({ name, value, style }) {
   return (
@@ -11,22 +11,65 @@ function StatusItem({ name, value, style }) {
   );
 }
 
-export default function Status({ compilerState, serverAddress, width }) {
-  return (
-    <Text style={styles.container}>
-      <StatusItem
-        name="state"
-        value={compilerState}
-        style={{ marginBottom: 1, marginLeft: 7 }}
-      />
-      <StatusItem name="listening on" value={serverAddress} />
-      <Text
-        style={{ ...styles.progressContainer, left: Math.floor(width / 2) }}
-      >
-        <Text>Progress bars will be here</Text>
+export default class Status extends React.Component {
+  state = {
+    iosCompilationProgress: 0,
+    androidCompilationProgress: 0,
+  };
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState(state => ({
+        iosCompilationProgress: state.iosCompilationProgress + 0.05,
+        androidCompilationProgress: state.iosCompilationProgress + 0.1,
+      }));
+
+      if (
+        this.state.iosCompilationProgress >= 1 &&
+        this.state.androidCompilationProgress >= 1
+      ) {
+        clearInterval(this.intervalId);
+      }
+    }, 800);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  render() {
+    const { compilerState, serverAddress, width } = this.props;
+    return (
+      <Text style={styles.container}>
+        <StatusItem
+          name="state"
+          value={compilerState}
+          style={{ marginBottom: 1, marginLeft: 7 }}
+        />
+        <StatusItem name="listening on" value={serverAddress} />
+        <Text
+          style={{ ...styles.progressContainer, left: Math.floor(width / 2) }}
+        >
+          <Text>
+            <ProgressBar
+              value={this.state.iosCompilationProgress}
+              barWidth={30}
+            />
+            <Text style={{ color: 'blue', display: 'inline' }}>{' iOS'}</Text>
+          </Text>
+          <Text>
+            <ProgressBar
+              value={this.state.androidCompilationProgress}
+              barWidth={30}
+            />
+            <Text style={{ color: 'blue', display: 'inline' }}>
+              {' Android'}
+            </Text>
+          </Text>
+        </Text>
       </Text>
-    </Text>
-  );
+    );
+  }
 }
 
 const styles = {
