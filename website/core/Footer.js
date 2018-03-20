a9f8d22ce28e6b5fd0cbc5ee1b0412420dd1181d
Paweł Trysła <pawel.trysla@gmail.com>
2018-03-20 13:36:57 +0100

    finish docs

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -23,30 +23,7 @@ class Footer extends React.Component {
     return (
       <footer className="nav-footer" id="footer">
         <section className="sitemap">
-          <a href={this.props.config.baseUrl} className="nav-home">
-            {this.props.config.footerIcon && (
-              <img
-                src={this.props.config.baseUrl + this.props.config.footerIcon}
-                alt={this.props.config.title}
-                width="66"
-                height="58"
-              />
-            )}
-          </a>
           <div>
-            <h5>Docs</h5>
-            <a href={this.docUrl('doc1.html', this.props.language)}>
-              Getting Started (or other categories)
-            </a>
-            <a href={this.docUrl('doc2.html', this.props.language)}>
-              Guides (or other categories)
-            </a>
-            <a href={this.docUrl('doc3.html', this.props.language)}>
-              API Reference (or other categories)
-            </a>
-          </div>
-          <div>
-            <h5>More</h5>
             <a href={this.props.config.repoUrl}>GitHub</a>
             <a
               className="github-button"
