10b74d27d9cce0a253392dbaf889362c644aa883
Paweł Trysła <pawel.trysla@gmail.com>
2018-05-04 18:36:30 +0200

    use WeakMap to support clean up of multiple different streams

--- packages/react-slate/src/components/KeyPress.js
+++ packages/react-slate/src/components/KeyPress.js
@@ -26,7 +26,7 @@ const INTERNAL = Symbol('KeyPress_Internal');
 export default class KeyPress extends React.Component<Props> {
   // $FlowFixMe
   static [INTERNAL] = {
-    instanceCount: 0,
+    streams: new WeakMap(),
   };
 
   onKeyPress = (ch: string, key: Key) => {
@@ -41,29 +41,52 @@ export default class KeyPress extends React.Component<Props> {
     if (!disableStreamCleanup) {
       stream.resume();
     }
+
+    // If stream wasn't stored, then the stream was not configured yet.
     // $FlowFixMe
-    if (KeyPress[INTERNAL].instanceCount === 0) {
+    if (!KeyPress[INTERNAL].streams.has(stream)) {
       setRawMode(stream, true);
       readline.emitKeypressEvents(stream);
     }
     stream.addListener('keypress', this.onKeyPress);
+
+    // Store stream and set initial value to 1, so another instance
+    // won't run configuration code again.
     // $FlowFixMe
-    KeyPress[INTERNAL].instanceCount++;
+    if (!KeyPress[INTERNAL].streams.has(stream)) {
+      // $FlowFixMe
+      KeyPress[INTERNAL].streams.set(stream, 1);
+    } else {
+      // $FlowFixMe
+      const count = KeyPress[INTERNAL].streams.get(stream);
+      // $FlowFixMe
+      KeyPress[INTERNAL].streams.set(stream, count + 1);
+    }
   }
 
   componentWillUnmount() {
     const { stream, disableStreamCleanup } = this.props;
     stream.removeListener('keypress', this.onKeyPress);
+
+    // Decrement number of instances of `KeyPress` operating on given stream,
+    // only if the stream was configured and stored.
     // $FlowFixMe
-    KeyPress[INTERNAL].instanceCount--;
-    // $FlowFixMe
-    if (KeyPress[INTERNAL].instanceCount === 0) {
-      setRawMode(stream, false);
-      if (!disableStreamCleanup) {
-        // This needs to be explicitly called, since `readline.emitKeypressEvents`
-        // contains side effects, which prevents node process from exiting.
-        // All code after this line will execute properly.
-        stream.pause();
+    if (KeyPress[INTERNAL].streams.has(stream)) {
+      // $FlowFixMe
+      const count = KeyPress[INTERNAL].streams.get(stream) - 1;
+      // $FlowFixMe
+      KeyPress[INTERNAL].streams.set(stream, count);
+
+      // Cleanup the stream if current instance was the only one left
+      // operating on given stream.
+      if (count === 0) {
+        setRawMode(stream, false);
+        if (!disableStreamCleanup) {
+          // This needs to be explicitly called, since `readline.emitKeypressEvents`
+          // contains side effects, which prevents node process from exiting.
+          // All code after this line will execute properly.
+          stream.pause();
+        }
       }
     }
   }
