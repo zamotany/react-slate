611fb1489e664df9c088b3423adbaa00f4f7b2ed
zamotany <zamotany.oss@gmail.com>
2019-10-12 00:34:31 +0200

    add render function to render dynamic and static content

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,6 +3,7 @@ export { terminal } from 'terminal-kit';
 export { default as renderToJson } from './renderToJson';
 export { default as renderToString } from './renderToString';
 export { default as renderFullscreen, exit } from './renderFullscreen';
+export { default as render, withRenderStatic, useRenderStatic } from './render';
 
 export { default as View, Props as ViewProps } from './components/View';
 export { default as Text, Props as TextProps } from './components/Text';
---
+++ packages/core/src/render.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { terminal } from 'terminal-kit';
+import renderToString from './renderToString';
+
+const getCursorLocation = () =>
+  (terminal.getCursorLocation as () => Promise<{
+    x: number;
+    y: number;
+  }>)();
+
+export type RenderStatic = (staticElement: JSX.Element) => void;
+
+const RenderStaticContext = React.createContext<{ renderStatic: RenderStatic }>(
+  {
+    renderStatic: () => {
+      /* NOOP */
+    },
+  }
+);
+
+type ComponentProps = { renderStatic: RenderStatic };
+
+export function withRenderStatic<P extends ComponentProps>(
+  Component: React.ComponentType<P>
+) {
+  function Wrapped(props: Pick<P, Exclude<keyof P, keyof ComponentProps>>) {
+    return (
+      <RenderStaticContext.Consumer>
+        {context => {
+          const newProps: P = {
+            ...props,
+            renderStatic: context.renderStatic,
+          } as any;
+          return <Component {...newProps} />;
+        }}
+      </RenderStaticContext.Consumer>
+    );
+  }
+
+  Wrapped.name = `withRenderStatic(${Component.displayName ||
+    Component.name ||
+    'Unknown'})`;
+
+  return Wrapped;
+}
+
+export function useRenderStatic() {
+  return React.useContext(RenderStaticContext).renderStatic;
+}
+
+export default function render(element: JSX.Element) {
+  let dynamicContentFirstRender = true;
+  let dynamicContentSnapshot = '';
+  let dynamicContentTop = 0;
+  let dynamicContentBottom = 0;
+
+  function renderStatic(staticElement: JSX.Element) {
+    const { snapshot: staticElementSnapshot } = renderToString(staticElement);
+    terminal.moveTo(0, dynamicContentTop);
+    terminal(staticElementSnapshot);
+    terminal('\n');
+    terminal(dynamicContentSnapshot);
+  }
+
+  (async () => {
+    for await (const snapshot of renderToString(
+      <RenderStaticContext.Provider value={{ renderStatic }}>
+        {element}
+      </RenderStaticContext.Provider>,
+      {
+        width: terminal.width,
+      }
+    )) {
+      dynamicContentSnapshot = snapshot || '';
+      if (dynamicContentFirstRender) {
+        dynamicContentFirstRender = false;
+        terminal(snapshot);
+        const { y } = await getCursorLocation();
+        dynamicContentBottom = y;
+      } else {
+        const dynamicConentHeight = (snapshot || '').split('\n').length;
+        dynamicContentTop = dynamicContentBottom - dynamicConentHeight + 1;
+        terminal.moveTo(0, dynamicContentTop);
+        terminal.eraseDisplayBelow();
+        terminal(snapshot);
+        const { y } = await getCursorLocation();
+        // eslint-disable-next-line require-atomic-updates
+        dynamicContentBottom = y;
+      }
+    }
+  })().catch(error => {
+    console.error(error);
+    terminal.processExit(1);
+  });
+
+  return { renderStatic };
+}
