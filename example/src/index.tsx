44bd4c3142920601f5fc2f324183b4fbee88833c
Paweł Trysła <zamotany@users.noreply.github.com>
2019-11-10 05:06:37 -0800

    chore: dash example (#110)

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,84 +1,143 @@
 import React from 'react';
-import { View, Text, Progress, render } from '../../packages/core';
+import {
+  View,
+  Text,
+  Progress,
+  terminal,
+  render,
+  renderToString,
+  useRenderStatic,
+} from '../../packages/core';
+import { EventEmitter } from 'events';
+import DayJs from 'dayjs';
 
-function Section({
-  label,
-  children,
-  growable,
-}: {
-  growable?: boolean;
-  label: string;
-  children: JSX.Element | JSX.Element[];
-}) {
+function Log({ children }: { children: string }) {
+  const lines = children.split('\n');
   return (
-    <View
-      width="100%"
-      paddingLeft={2}
-      paddingRight={2}
-      flexDirection="column"
-      flexGrow={growable ? 1 : undefined}
-    >
-      <Text bold color="magenta">
-        [{label}]
-      </Text>
-      <View
-        width="100%"
-        height="100%"
-        paddingLeft={2}
-        paddingRight={2}
-        paddingTop={1}
-        paddingBottom={1}
-        flexDirection="column"
-      >
-        {children}
-      </View>
-    </View>
-  );
-}
-
-function CompilationStatus({
-  label,
-  status,
-  progress,
-}: {
-  label: string;
-  status: string;
-  progress: number;
-}) {
-  return (
-    <View width="100%">
+    <View>
       <View marginRight={1}>
-        <Text bold color="blue">
-          {label}
+        <Text bold color="gray">
+          [{DayJs().format('HH:mm:ss:SSS')}]
         </Text>
       </View>
       <View marginRight={1}>
-        <Text italic>{status}</Text>
+        <Text bold>info</Text>
       </View>
-      <Progress value={progress} percentage />
+      <View width={1}>
+        <Text>▶︎</Text>
+      </View>
+      <View marginLeft={1} flexDirection="column">
+        {lines.map((line, i) => (
+          <View key={i}>
+            <Text>{line}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
+
+function Compilation({
+  label,
+  maxLabelWidth,
+  value,
+}: {
+  label: string;
+  maxLabelWidth: number;
+  value: number;
+}) {
+  return (
+    <View width="100%">
+      <View minWidth={maxLabelWidth}>
+        <Text bold>{label}</Text>
+      </View>
+      <View marginLeft={2} marginRight={2} minWidth={7}>
+        <Text italic>{value === 0 ? 'idle' : 'running'}</Text>
+      </View>
+      <Progress value={value} percentage />
+    </View>
+  );
+}
+
+function Dash({ compiler }: { compiler: EventEmitter }) {
+  const renderStatic = useRenderStatic();
+  const [state, setState] = React.useState({ ios: 0, android: 0 });
+
+  React.useEffect(() => {
+    compiler.on('progress', ({ platform, value }) => {
+      setState(state => ({ ...state, [platform]: value }));
+    });
+
+    compiler.on('log', ({ message }) => {
+      renderStatic(<Log>{message}</Log>);
+    });
+
+    return () => {
+      compiler.removeAllListeners();
+    };
+  }, [compiler, renderStatic]);
+
+  return (
+    <View flexDirection="column" paddingTop={1} width="100%">
+      <Text color="green" bold>
+        Compilations:
+      </Text>
+      <View flexDirection="column" width="90%">
+        <Compilation label="iOS" maxLabelWidth={7} value={state.ios} />
+        <Compilation label="Android" maxLabelWidth={7} value={state.android} />
+      </View>
+    </View>
+  );
+}
+
 class App extends React.Component {
-  state = {};
+  compiler = new EventEmitter();
+  logsTimeout?: NodeJS.Timeout;
+  progressTimeout?: NodeJS.Timeout;
+
+  counter = 0;
+
+  emitLogs = () => {
+    this.logsTimeout = setTimeout(() => {
+      this.compiler.emit('log', {
+        message: `New log ${this.counter}${
+          this.counter % 3 === 0 ? '\nNew line boom' : ''
+        }`,
+      });
+      this.counter++;
+      this.emitLogs();
+    }, Math.random() * (5000 - 1000) + 1000);
+  };
+
+  emitProgress = (platform: string, value: number = 0) => {
+    this.progressTimeout = setTimeout(() => {
+      this.compiler.emit('progress', { platform, value: value + 0.05 });
+      this.emitProgress(platform, value + 0.05);
+    }, Math.random() * (500 - 10) + 10);
+  };
+
+  componentDidMount() {
+    this.emitLogs();
+    this.emitProgress('ios');
+  }
+
+  componentWillUnmount() {
+    this.logsTimeout && clearTimeout(this.logsTimeout);
+    this.progressTimeout && clearTimeout(this.progressTimeout);
+  }
 
   render() {
-    return (
-      <View width="100%" height="100%" flexDirection="column">
-        <Section label="Compilation">
-          <CompilationStatus label="ASDF " status="running" progress={0.2} />
-          <CompilationStatus label="QWERT" status="running" progress={0.6} />
-        </Section>
-        <Section label="Logs" growable>
-          <View bgColor="blue" width="100%" height="100%" testID="container">
-            <View marginTop={0} testID="textBox">
-              <Text>This should take the remaining space</Text>
-            </View>
-          </View>
-        </Section>
-      </View>
-    );
+    return <Dash compiler={this.compiler} />;
   }
 }
 
+terminal(
+  renderToString(
+    <View marginBottom={1}>
+      <Text bold color="green">
+        Starting...
+      </Text>
+    </View>
+  ).snapshot
+);
 render(<App />);
